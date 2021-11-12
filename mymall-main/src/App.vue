08e5690bcc8cc40a038761ebdfdc3fd7f7c39f77
小町 <87963792+Gy3186182625@users.noreply.github.com>
2021-11-12 19:48:16 +0800

    Add files via upload

---
+++ mymall-main/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <keep-alive exclude="Detail">
+      <router-view></router-view>
+    </keep-alive>
+    <main-tab-bar></main-tab-bar>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from '@/components/content/tabbar/MainTabBar'
+
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    },
+    // provide(){
+    //   return{
+    //     reload: this.reload
+    //   }
+    // },
+    // data(){
+    //   return{
+    //     isRouterAlive: true
+    //   }
+    // },
+    // methods:{
+    //   reload(){
+    //     this.isRouterAlive = false
+    //     this.$nextTick(function () {
+    //       this.isRouterAlive = true
+    //     })
+    //   }
+    // }
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+
+</style>
